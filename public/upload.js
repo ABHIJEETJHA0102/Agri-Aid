2658ea43d97afb80ea86d1641b60885539f221ec
deathgod_0809 <b22149@students.iitmandi.ac.in>
2024-04-24 12:19:40 +0530

    modified upload_image

--- public/upload.js
+++ public/upload.js
@@ -1,49 +1,130 @@
-/*=============== SHOW MENU ==========*/
-const navMenu = document.getElementById('nav__menu'),navToggle = document.getElementById('nav__toggle'),navClose = document.getElementById('nav__close')
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+// =============upload button==========
+const fileInput = document.getElementById("file-upload");
+const fileNameDiv = document.querySelector(".file-name");
 
+fileInput.addEventListener("change", () => {
+  const files = fileInput.files;
+  if (files.length > 0) {
+    const fileName = files[0].name;
+    fileNameDiv.textContent = fileName;
+  } else {
+    fileNameDiv.textContent = "";
+  }
+});
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
 if(navToggle){
-    navToggle.addEventListener('click',()=>{
+    navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
     })
 }
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
 if(navClose){
-    navClose.addEventListener('click',()=>{
+    navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
 }
+
 /*=============== REMOVE MENU MOBILE ===============*/
-const navLink= document.querySelectorAll('.nav__link')
-const linkAction = ()=>{
-    const navMenu= document.getElementById('nav__menu')
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
-navLink.forEach(n=> n.addEventListener('click',linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*=============== ADD BLUR TO HEADER ===============*/
 
-const blurHeader =()=>{
-    const header= document.getElementById('header')
-    this.scrollY >=50 ? header.classList.add('blur-header')
-                       : header.classList.remove('blur-header')
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 80) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
-window.addEventListener('scroll', blurHeader)
+window.addEventListener('scroll', scrollHeader)
 
 
-const scrollUp =()=>{
-    const scrollUp= document.getElementById('scroll-up')
-    this.scrollY >=350 ? scrollUp.classList.add('show-scroll')
-                       : scrollUp.classList.remove('show-scroll')
+
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 400 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+  // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
 }
 
-
-const sr = ScrollReveal({
-    origin:'top',
-    distance:'60px',
-    duration:3000,
-    delay:400,
-     reset:true // animation repeat
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
 })
-sr.reveal(".home--data,.footer__container")
 
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
+})
 
-sr.reveal('.new__card',{interval:200})
+sr.reveal(`.home__data`)
+sr.reveal(`.home__img`, {delay: 500})
+sr.reveal(`.home__social`, {delay: 600})
+
+sr.reveal(`.steps__card, .footer`,{interval: 100})
