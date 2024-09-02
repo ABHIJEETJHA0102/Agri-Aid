2f8a51852b9c8211f06bac443bfb2dc59294ebf1
deathgod_0809 <b22149@students.iitmandi.ac.in>
2024-09-02 23:07:47 +0530

    Updated code

--- scripts/predict.py
+++ scripts/predict.py
@@ -24,29 +24,29 @@ def main2():
     # sys.stdout.write("called")
     # Resize the image to the expected input size
     arr=arr = ['Apple__Apple_scab', 'Apple_Black_rot', 'Apple_Cedar_apple_rust', 'Apple_healthy', 'Blueberry_healthy', 'Cherry(including_sour)__healthy', 'Cherry(including_sour)__Powdery_mildew', 'Corn(maize)__Cercospora_leaf_spot Gray_leaf_spot', 'Corn(maize)__Common_rust', 'Corn_(maize)__healthy', 'Corn(maize)__Northern_Leaf_Blight', 'Grape_Black_rot', 'Grape_Esca(Black_Measles)', 'Grape__healthy', 'Grape_Leaf_blight(Isariopsis_Leaf_Spot)', 'Orange__Haunglongbing(Citrus_greening)', 'Peach__Bacterial_spot', 'Peach_healthy', 'Pepper,_bell_Bacterial_spot', 'Pepper,_bell_healthy', 'Potato_Early_blight', 'Potato_healthy', 'Potato_Late_blight', 'Raspberry_healthy', 'Soybean_healthy', 'Squash_Powdery_mildew', 'Strawberry_healthy', 'Strawberry_Leaf_scorch', 'Tomato_Bacterial_spot', 'Tomato_Early_blight', 'Tomato_healthy', 'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot', 'Tomato_Spider_mites Two-spotted_spider_mite', 'Tomato_Target_Spot', 'Tomato_Tomato_mosaic_virus', 'Tomato__Tomato_Yellow_Leaf_Curl_Virus']
-    # image_path = "./scripts/image.jpg"
-    # img = image.load_img(image_path, target_size=(224, 224))
-    # img_array = image.img_to_array(img)
-    # img_array = np.expand_dims(img_array, axis=0)
-    # Get the base64 image data from the command line arguments
-    image_data = sys.argv[1]
-
-    # Decode the base64 string to bytes
-    try:
-        image_bytes = base64.b64decode(image_data)
-    except Exception as e:
-        print(f"Error decoding base64 string: {e}")
-        return
-
-    # Convert bytes to a NumPy array
-    img_array = np.frombuffer(image_bytes, dtype=np.uint8)
-
-    # Load the image using Keras
-    img = image.array_to_img(img_array)
-    img = img.resize((224, 224))  # Resize to target size
+    image_path = "./scripts/image.jpg"
+    img = image.load_img(image_path, target_size=(224, 224))
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
-    print("Image successfully processed.")
+    # Get the base64 image data from the command line arguments
+    # image_data = sys.argv[1]
+
+    # # Decode the base64 string to bytes
+    # try:
+    #     image_bytes = base64.b64decode(image_data)
+    # except Exception as e:
+    #     print(f"Error decoding base64 string: {e}")
+    #     return
+
+    # # Convert bytes to a NumPy array
+    # img_array = np.frombuffer(image_bytes, dtype=np.uint8)
+
+    # # Load the image using Keras
+    # img = image.array_to_img(img_array)
+    # img = img.resize((224, 224))  # Resize to target size
+    # img_array = image.img_to_array(img)
+    # img_array = np.expand_dims(img_array, axis=0)
+    # print("Image successfully processed.")
 
     # Preprocess the input
     img_array_copy = img_array.copy()  # Create a copy of the array
